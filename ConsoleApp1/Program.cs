a2f980d4fb38d7de4827a0c8dcef6681cedd2d08
Ramses Sanchez <ramsessanchez25@gmail.com>
2025-02-12 23:53:21 -0500

    First Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true) // Loop until the user chooses to quit
+        {
+            
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1 - New Game");
+            Console.WriteLine("2 - Load Game");
+            Console.WriteLine("3 - Options");
+            Console.WriteLine("4 - Quit");
+            
+            Console.Write("Enter your choice: ");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+                Console.WriteLine("Starting a new game...\n\n");
+            else if (choice == "2")
+                Console.WriteLine("Loading game...\n\n");
+            else if (choice == "3")
+                Console.WriteLine("Opening options...\n\n");
+            else if (choice == "4")
+            {
+                Console.WriteLine("Goodbye!\n\n");
+                break; // Exit the loop to quit the program
+            }
+            else
+                Console.WriteLine("Invalid choice. Please try again.\n\n");
+        }
+    }
+}
